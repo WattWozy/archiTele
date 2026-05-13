58478746c626c5c75bdf8aabc3b33eec2eb33c07
WattWozy <niklas2@gmx.es>
2026-05-13 23:10:53 +0200

    dummy ts

---
+++ frontend/domain/Entity.ts
@@ -0,0 +1,12 @@
+export interface Entity {
+  id: string;
+  createdAt: Date;
+}
+
+export class BaseEntity implements Entity {
+  constructor(public readonly id: string, public readonly createdAt: Date) {}
+
+  equals(other: Entity): boolean {
+    return this.id === other.id;
+  }
+}
